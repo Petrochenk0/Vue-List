eb70cbc51a9c3058cc2fc41afd2298d76225fb64
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2023-02-26 12:28:44 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -36,8 +36,10 @@
 </template>
 
 <script>
+
 // import PostForm from '@/components/PostForm.vue';
 // import PostList from '@/components/PostList.vue';
+
 export default{
     // components:{
     //   PostForm, PostList 
